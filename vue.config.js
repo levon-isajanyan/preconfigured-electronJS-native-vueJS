3be71ca7d3dfaba9ee5e70cc9112da343b277096
Levon Isajanyan <levonisadjanyan@gmail.com>
2019-12-09 14:28:27 +0400

    COnfig row for production rendering electron app in dev mode

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 module.exports = {
-  
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      config.output.publicPath = `${process.cwd()}/dist/`
+    }
+  },
   pluginOptions: {
     'style-resources-loader': {
       'preProcessor': 'scss',
